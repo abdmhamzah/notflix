e27671aa26003aa05e246679f3f20a6aaf621753
abdmhamzah <iamhamzaham@gmail.com>
2020-05-10 10:53:29 +0700

    setup routing in client done

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { ApolloProvider } from "@apollo/react-hooks";
+import client from "./graphql/graphql";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import { Home, Movies, TvSeries } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ApolloProvider client={client}>
+        <Router>
+          <nav>
+            <Link to="/entertainme">Home</Link>
+            <Link to="/movies">Movies</Link>
+            <Link to="/tv">Tv Series</Link>
+          </nav>
+          <Switch>
+            <Route path="/entertainme" component={Home} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/tv" component={TvSeries} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
+      </ApolloProvider>
     </div>
   );
 }
